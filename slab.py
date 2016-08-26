97f114676bd8b2b515e45bb93d33084ca38e2707
Andrew Burnett <andrew@andburn.info>
2016-08-26 17:44:28 +0100

    Update slab.py to parse and output to file

--- slab.py
+++ slab.py
@@ -1,19 +1,25 @@
 import sys
 import os
-import mojoshader
-import shaderlab
-import ctypes
+from mojoshader import Parser, ShaderType
+from shaderlab import Blob
+from ctypes import cast, c_char_p
 
 
-with open(sys.argv[1], "rb") as f:
-	data = f.read()
+with open(sys.argv[1], "rb") as fi:
+	data = fi.read()
+	p = Parser()
+	b = Blob(data)
 
-p = mojoshader.Parser()
-b = shaderlab.Blob(data)
-for i, (t, s) in enumerate(b.shaders):
-	m = p.parse(s)
-	if m.error_count > 0:
-		print(m.errors.contents.error)
-	print(m.profile, m.error_count, m.output_len)
-	print(t)
-	print(ctypes.cast(m.output, ctypes.c_char_p).value)
+	for i, (t, s) in enumerate(b.shaders):
+		m = p.parse(s)
+		if m.error_count > 0:
+			print(m.errors.contents.error)
+		fname = "-".join(t)
+		if m.shader_type == ShaderType.PIXEL:
+			fname = "PS_" + fname
+		elif m.shader_type == ShaderType.VERTEX:
+			fname = "VS_" + fname
+
+		out_file = os.path.join(sys.argv[2], fname + ".cg")
+		with open(out_file, "w") as fo:
+			fo.write(cast(m.output, c_char_p).value.decode("utf-8"))
